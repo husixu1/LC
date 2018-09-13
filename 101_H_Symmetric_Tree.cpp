3387297632bec73d2eea283e360b2d840f2c8969
Sixu Hu <husixu1@hotmail.com>
2018-09-14 01:05:40 +0800

    101 finished

---
+++ 101_H_Symmetric_Tree.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(!root)
+            return true;
+        return helper(root->left, root->right);
+    }
+    bool helper(TreeNode *l, TreeNode *r){
+        if(!l && !r)
+            return true;
+        if(!(l && r))
+            return false;
+        if(l->val != r->val)
+            return false;
+        return helper(l->left, r->right) && helper(l->right, r->left);
+    }
+};
